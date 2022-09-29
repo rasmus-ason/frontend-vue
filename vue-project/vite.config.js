9044b66674e33354f58ad4a8384bbcc23e92c041
Rasmus Andersson <90144367+rasmus-ason@users.noreply.github.com>
2022-09-29 13:21:03 +0200

    uppdat

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -5,8 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //Search path
-  base: '/frontend-vue/tree/gh-pages/vue-project',
+  
   plugins: [vue()],
   resolve: {
     alias: {
